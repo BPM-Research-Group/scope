6d3d63e05edbcb84b85b23941ea5fcb541006a5a
MauriceChartee <maurice.chartee@rwth-aachen.de>
2025-07-24 23:49:37 +0200

    refactored main and made it simpler

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,12 @@
-use axum::{routing::get, routing::post, Router};
-use crate::models::ocel::{OcelJson};
-use crate::handlers::ocel_post;
+mod routes;
+mod handlers;
+mod models;
+mod core;
 
 #[tokio::main]
 pub async fn main() {
-    let router = Router::new().route("/upload", post(ocel_post));
-
-    let addr = "0.0.0.0:3000";
+    let app = routes::create_routes();
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-    axum::serve(listener, router).await.unwrap();
-    
+    axum::serve(listener, app).await.unwrap();
 }
