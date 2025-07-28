10b234d478882aadfbcbc5f63d43d1f32dca9538
MauriceChartee <maurice.chartee@rwth-aachen.de>
2025-07-28 09:48:23 +0200

    added cors so backend can receive messages from localhost:5173

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,7 +1,16 @@
 pub mod upload;
 use axum::Router;
+use tower_http::cors::{CorsLayer, Any};
+use axum::http::Method;
+use axum::http::HeaderValue;
 
 pub fn create_routes() -> Router {
+    let cors = CorsLayer::new()
+        .allow_origin(HeaderValue::from_static("http://localhost:5173")) // frontend origin
+        .allow_methods([Method::GET, Method::POST, Method::DELETE, Method::PUT])
+        .allow_headers(Any);
+
     Router::new()
         .nest("/upload", upload::router())
+        .layer(cors)
 }
