a90b2349f1ec34785c5084e8760b1c13daaf1cbf
MauriceChartee <maurice.chartee@rwth-aachen.de>
2025-08-06 13:09:24 +0200

    added route for delete functionality

--- backend/src/routes/v1/objects.rs
+++ backend/src/routes/v1/objects.rs
@@ -1,14 +1,16 @@
 use axum::{
     Router,
-    routing::{get},
+    routing::{get, delete},
 
 };
-use crate::handlers::ocpt::{get_ocpt};
-use crate::handlers::ocel::{get_ocel};
+use crate::handlers::ocpt::{get_ocpt,delete_ocpt};
+use crate::handlers::ocel::{get_ocel,delete_ocel};
 
 
 pub fn router() -> Router {
     Router::new()
         .route("/ocel/{file_id}", get(get_ocel))
         .route("/ocpt/{file_id}", get(get_ocpt))
+        .route("/ocel/{file_id}", delete(delete_ocel))
+        .route("/ocpt/{file_id}", delete(delete_ocpt))
 }
