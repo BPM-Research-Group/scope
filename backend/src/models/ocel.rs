aa164f821935f0973cd3a503885d6e9280952ae6
MauriceChartee <maurice.chartee@rwth-aachen.de>
2025-09-01 12:10:01 +0200

    added ocel crates from rust4pm

--- backend/src/models/ocel.rs
+++ backend/src/models/ocel.rs
@@ -1,4 +1,10 @@
+pub use process_mining::ocel::linked_ocel;
 pub use process_mining::ocel::ocel_struct::{
     OCEL, OCELType, OCELTypeAttribute, OCELEvent, OCELEventAttribute, OCELObject,
     OCELObjectAttribute, OCELRelationship, OCELAttributeValue, OCELAttributeType,
 };
+pub use process_mining::ocel::linked_ocel::{
+    IndexLinkedOCEL, LinkedOCELAccess
+};
+
+pub use process_mining::ocel::linked_ocel::index_linked_ocel::{EventIndex, ObjectIndex};
