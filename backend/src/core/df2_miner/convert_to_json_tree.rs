d1679e411413972de35ac6ed82ffc41a6f57ca68
MauriceChartee <maurice.chartee@rwth-aachen.de>
2025-09-19 17:00:03 +0200

    changed structs to be public

--- backend/src/core/df2_miner/convert_to_json_tree.rs
+++ backend/src/core/df2_miner/convert_to_json_tree.rs
@@ -10,7 +10,7 @@ pub struct OutputJson {
 
 #[derive(Serialize)]
 #[serde(untagged)]
-enum HierarchyNode {
+pub enum HierarchyNode {
     Operator {
         value: String,
         children: Vec<HierarchyNode>,
@@ -22,7 +22,7 @@ enum HierarchyNode {
 
 #[derive(Serialize)]
 #[allow(non_snake_case)] // for isSilent, can't resolve warning since the name is required like this in the frontend
-struct ActivityValue {
+pub struct ActivityValue {
     #[serde(skip_serializing_if = "Option::is_none")]
     isSilent: Option<bool>,
     activity: String,
