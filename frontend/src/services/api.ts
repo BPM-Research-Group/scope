2261470e7409c4d1dca241cdd8d1a89da3bb8dd9
Annika Kröning <akinna@outlook.com>
2026-08-16 21:41:06 +0200

    noise backend included

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -304,7 +304,7 @@ export const materialiseClustering = async (case_ocels_file_id: string, case_ass
     return response;
 };
 
-export const labelSplitting = async (case_ocels_file_id: string, eps: any, min_samples: any) => {
-    const response = await api.post(`/v1/activity_label_splitting/${case_ocels_file_id}`, { params: { eps, min_samples } });
+export const labelSplitting = async (case_ocels_file_id: string, eps: any, min_samples: any, keep_noise: boolean) => {
+    const response = await api.post(`/v1/activity_label_splitting/${case_ocels_file_id}`, { params: { eps, min_samples, keep_noise } });
     return response;
 }
